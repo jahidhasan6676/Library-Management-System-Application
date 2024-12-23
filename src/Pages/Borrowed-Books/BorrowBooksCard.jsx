ca1e4a55f1c4b150236b350b7ed4d69c926aa0d7
jahidhasan6676 <jh18186676@gmail.com>
2024-12-23 13:09:43 +0600

    borrow books list UI delete and books quantity increase

--- src/Pages/Borrowed-Books/BorrowBooksCard.jsx
+++ src/Pages/Borrowed-Books/BorrowBooksCard.jsx
@@ -1,7 +1,8 @@
 
 
-const BorrowBooksCard = ({ borrow }) => {
-    const { _id, bookName, authorName, quantity, category, image } = borrow || {};
+const BorrowBooksCard = ({ borrow, handleDelete }) => {
+    const { _id, bookName, authorName, quantity, category, image, bookId } = borrow || {};
+    console.log(borrow)
     return (
         <div className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300">
 
@@ -17,6 +18,7 @@ const BorrowBooksCard = ({ borrow }) => {
                 <p className="text-sm text-gray-600">Author: {authorName}</p>
                 <p className="text-sm text-gray-600">Category: {category}</p>
                 <button
+                    onClick={()=>handleDelete(_id, bookId)}
                     className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300">Return</button>
               
             </div>
--- src/Pages/Borrowed-Books/BorrowedBooks.jsx
+++ src/Pages/Borrowed-Books/BorrowedBooks.jsx
@@ -2,28 +2,43 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import UseAuth from "../../Hooks/UseAuth";
 import BorrowBooksCard from "./BorrowBooksCard";
+import { toast } from "react-toastify";
 
 
 const BorrowedBooks = () => {
-    const {user} = UseAuth();
+    const { user } = UseAuth();
     const [borrowBooks, setBorrowBooks] = useState([]);
-    useEffect(()=>{
-      fetchBorrowBooks()
-       
-    },[user?.email]);
-  
-    const fetchBorrowBooks = async ()=>{
-      const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/borrow/${user?.email}`)
-      setBorrowBooks(data)
-     
+    useEffect(() => {
+        fetchBorrowBooks()
+
+    }, [user?.email]);
+
+    const fetchBorrowBooks = async () => {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/borrow/${user?.email}`)
+        setBorrowBooks(data)
+
     }
-  console.log(borrowBooks)
+
+    // delete user add borrow books
+    const handleDelete = async (id, bookId) => {
+
+        try {
+            const { data } = await axios.delete(`${import.meta.env.VITE_API_URL}/borrow/${id}?bookId=${bookId}`)
+            console.log(data)
+            fetchBorrowBooks()
+            toast.success("Data Successfully Deleted");
+          
+        } catch (err) {
+            console.log(err)
+            toast.error(err.message)
+        }
+    }
     return (
         <div className="bg-gray-100 py-16">
             <h2 className="text-center text-2xl font-bold">My all borrow books</h2>
             <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
                 {
-                    borrowBooks.map(borrow => <BorrowBooksCard key={borrow._id} borrow={borrow}></BorrowBooksCard>)
+                    borrowBooks.map(borrow => <BorrowBooksCard key={borrow._id} borrow={borrow} handleDelete={handleDelete}></BorrowBooksCard>)
                 }
             </div>
         </div>
