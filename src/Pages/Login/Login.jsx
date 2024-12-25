24be4f8503ba8931caf716d4418816802a33e012
jahidhasan6676 <jh18186676@gmail.com>
2024-12-25 18:59:44 +0600

    added the login and register page add lottie design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,11 +4,12 @@ import SocialLogin from "../../Shared/SocialAuth/SocialLogin";
 import UseAuth from "../../Hooks/UseAuth";
 import { useState } from "react";
 import { toast } from "react-toastify";
-
+import loginLottieData from "../../assets/lottie/lottie-login.json"
+import Lottie from "lottie-react";
 
 
 const Login = () => {
-    const {loginUser, setUser} = UseAuth();
+    const { loginUser, setUser } = UseAuth();
     const [error, setError] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
@@ -36,78 +37,85 @@ const Login = () => {
             })
 
     }
-    
+
     return (
         <div className="py-20 bg-gradient-to-br from-blue-100 via-purple-100 to-blue-50">
-            <div className="flex items-center justify-center   ">
-                <div className="w-11/12 lg:10/12 max-w-md p-6 bg-white rounded-lg shadow-lg">
-                    {/* Title */}
-                    <h2 className="text-3xl font-semibold text-center text-blue-600 mb-6">
-                        Login
-                    </h2>
-
-                    {/* Form */}
-                    <form onSubmit={handleLogin}  className="space-y-6">
-                        {/* Email Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Email Address
-                            </label>
-                            <input
-                                type="email"
-                                placeholder="Enter your email"
-                                name="email"
-                                required
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600 "
-                            />
-                        </div>
-
-                        {/* Password Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                placeholder="Enter your password"
-                                name="password"
-                                required
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
-                            />
-                            {
-                                error && <p className='text-xs text-red-500'>Invalid Email or Password! </p>
-                            }
-                        </div>
-
-                        {/* Forget Password */}
-                        <div className="text-right">
-                            <a
-                                href="#"
-                                className="text-sm text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            >
-                                Forgot Password?
-                            </a>
-                        </div>
-
-                        {/* Login Button */}
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 bg-blue-600 text-white font-medium text-lg rounded-lg hover:bg-blue-700 transition duration-300"
-                        >
+            <div className="w-11/12 lg:w-9/12 mx-auto space-y-8 lg:flex justify-around items-center">
+                <div className=" flex-1 ">
+                    <div className="lg:w-2/3 mx-auto  p-6 bg-white rounded-lg shadow-lg">
+                        {/* Title */}
+                        <h2 className="text-3xl font-semibold text-center text-blue-600 mb-6">
                             Login
-                        </button>
+                        </h2>
 
-                    </form>
-                    {/* Login Link */}
-                    <div className="mt-4 text-center">
-                        <p className="text-sm text-gray-600">
-                            Don’t have an account ?
-                            <Link to="/register" className="text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500">
-                                Register
-                            </Link>
-                        </p>
+                        {/* Form */}
+                        <form onSubmit={handleLogin} className="space-y-6">
+                            {/* Email Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Email Address
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    name="email"
+                                    required
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600 "
+                                />
+                            </div>
+
+                            {/* Password Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Enter your password"
+                                    name="password"
+                                    required
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
+                                />
+                                {
+                                    error && <p className='text-xs text-red-500'>Invalid Email or Password! </p>
+                                }
+                            </div>
+
+                            {/* Forget Password */}
+                            <div className="text-right">
+                                <a
+                                    href="#"
+                                    className="text-sm text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                >
+                                    Forgot Password?
+                                </a>
+                            </div>
+
+                            {/* Login Button */}
+                            <button
+                                type="submit"
+                                className="w-full py-2 px-4 bg-blue-600 text-white font-medium text-lg rounded-lg hover:bg-blue-700 transition duration-300"
+                            >
+                                Login
+                            </button>
+
+                        </form>
+                        {/* Login Link */}
+                        <div className="mt-4 text-center">
+                            <p className="text-sm text-gray-600">
+                                Don’t have an account ?
+                                <Link to="/register" className="text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                    Register
+                                </Link>
+                            </p>
+                        </div>
+                        <SocialLogin></SocialLogin>
                     </div>
-                    <SocialLogin></SocialLogin>
+                </div>
+
+
+                <div className="flex-1 max-w-md">
+                    <Lottie animationData={loginLottieData}></Lottie>
                 </div>
             </div>
         </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -4,11 +4,12 @@ import UseAuth from "../../Hooks/UseAuth";
 import { useState } from "react";
 import SocialLogin from "../../Shared/SocialAuth/SocialLogin";
 import { toast } from "react-toastify";
-
+import registerLottieData from '../../assets/lottie/lottie-register.json'
+import Lottie from "lottie-react";
 
 
 const Register = () => {
-    const {createUser, updateUserProfile, setUser} = UseAuth();
+    const { createUser, updateUserProfile, setUser } = UseAuth();
     const navigate = useNavigate();
     const [error, setError] = useState('');
 
@@ -40,7 +41,7 @@ const Register = () => {
             .then(result => {
                 console.log(result.user);
                 setUser(result.user);
-                updateUserProfile({displayName:name, photoURL:photo})
+                updateUserProfile({ displayName: name, photoURL: photo })
                 navigate("/")
                 toast.success("Successfully Register")
             })
@@ -49,96 +50,105 @@ const Register = () => {
             })
     }
 
-    
-    
+
+
     return (
         <div className="py-20 bg-gradient-to-br from-green-100 via-purple-100 to-blue-50">
-            <div className="flex items-center justify-center  ">
-                <div className="w-11/12 lg:w-10/12 max-w-md p-6 bg-white rounded-lg shadow-lg">
-                    {/* Title */}
-                    <h2 className="text-3xl font-semibold text-center text-blue-600 mb-6">
-                        Register
-                    </h2>
 
-                    {/* Registration Form */}
-                    <form onSubmit={handleRegister} className="space-y-6">
-                        {/* Name Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Name
-                            </label>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Enter your name"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
-                                required
-                            />
-                        </div>
 
-                        {/* Email Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Enter your email"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
-                                required
-                            />
-                        </div>
+            <div className="w-11/12 lg:w-9/12 mx-auto lg:flex justify-around items-center">
 
-                        {/* Photo URL Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Photo URL
-                            </label>
-                            <input
-                                type="url"
-                                name="photoURL"
-                                placeholder="Enter your photo URL"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
-                            />
-                        </div>
-
-                        {/* Password Field */}
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Enter your password"
-                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
-                                required
-                            />
-                            {
-                                error && <p className='text-xs text-red-500'>{error}</p>
-                            }
-                        </div>
-
-                        {/* Register Button */}
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 bg-blue-600 text-white font-medium text-lg rounded-lg hover:bg-blue-700">
+                <div className="flex-1">
+                    <div className="lg:w-5/6 mx-auto  p-6 bg-white rounded-lg shadow-lg">
+                        {/* Title */}
+                        <h2 className="text-3xl font-semibold text-center text-blue-600 mb-6">
                             Register
-                        </button>
-                    </form>
+                        </h2>
 
-                    {/* Login Link */}
-                    <div className="mt-4 text-center">
-                        <p className="text-sm text-gray-600">
-                            Already have an account?
-                            <Link to="/login" className="text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500">
-                                Login
-                            </Link>
-                        </p>
+                        {/* Registration Form */}
+                        <form onSubmit={handleRegister} className="space-y-6">
+                            {/* Name Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Enter your name"
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
+                                    required
+                                />
+                            </div>
+
+                            {/* Email Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter your email"
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
+                                    required
+                                />
+                            </div>
+
+                            {/* Photo URL Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Photo URL
+                                </label>
+                                <input
+                                    type="url"
+                                    name="photoURL"
+                                    placeholder="Enter your photo URL"
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
+                                />
+                            </div>
+
+                            {/* Password Field */}
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Enter your password"
+                                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none  focus:border-gray-600"
+                                    required
+                                />
+                                {
+                                    error && <p className='text-xs text-red-500'>{error}</p>
+                                }
+                            </div>
+
+                            {/* Register Button */}
+                            <button
+                                type="submit"
+                                className="w-full py-2 px-4 bg-blue-600 text-white font-medium text-lg rounded-lg hover:bg-blue-700">
+                                Register
+                            </button>
+                        </form>
+
+                        {/* Login Link */}
+                        <div className="mt-4 text-center">
+                            <p className="text-sm text-gray-600">
+                                Already have an account?
+                                <Link to="/login" className="text-blue-600 hover:underline focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                    Login
+                                </Link>
+                            </p>
+                        </div>
+
+                        <SocialLogin></SocialLogin>
                     </div>
-
-                    <SocialLogin></SocialLogin>
+                </div>
+ 
+                <div className="flex-1 max-w-md">
+                    <Lottie animationData={registerLottieData}></Lottie>
                 </div>
             </div>
         </div>
